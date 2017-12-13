7910c06564e4372c834436a14cc7b2df6cb03089
bemzj <qiuzijia520@qq.com>
2017-12-13 16:33:26 +0800

    xx

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('.pop1 .ok').bind('click',function(){
+		$('#pop1').hide();
+	});
+	$('.pop2 .ok').bind('click',function(){
+		$('#pop2').hide();
+	});
+	$('.pop3 .ok').bind('click',function(){
+		$('#pop3').hide();
+	});
+});
